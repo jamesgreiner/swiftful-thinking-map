9724ad4c9aeeb20baea2de85e5120e1554e27b0c
James Greiner <jamesdgreiner@icloud.com>
2023-07-12 23:56:16 -0700

    Add model for map locations

---
+++ SwiftfulThinkingMap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulThinkingMap
+//
+//  Created by James Greiner on 7/12/23.
+//
+
+import MapKit
+import Foundation
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
