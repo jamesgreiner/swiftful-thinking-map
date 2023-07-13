a58ef13ad7a762f3d4ba7f601ff5e8d24d6cd904
James Greiner <jamesdgreiner@icloud.com>
2023-07-12 22:34:19 -0700

    Initial Commit

---
+++ SwiftfulThinkingMap/SwiftfulThinkingMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingMapApp.swift
+//  SwiftfulThinkingMap
+//
+//  Created by James Greiner on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
