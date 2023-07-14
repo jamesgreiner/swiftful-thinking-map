0052140dac910ef2122da92b4e470d63b9304401
James Greiner <jamesdgreiner@icloud.com>
2023-07-13 21:43:35 -0700

    Add locations view

---
+++ SwiftfulThinkingMap/Views/LocationsView.swift
@@ -0,0 +1,27 @@
+//
+//  LocationsView.swift
+//  SwiftfulThinkingMap
+//
+//  Created by James Greiner on 7/13/23.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    var body: some View {
+        List {
+            ForEach(vm.locations) {
+                Text($0.name)
+            }
+        }
+    }
+}
+
+struct LocationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsView()
+            .environmentObject(LocationsViewModel())
+    }
+}
