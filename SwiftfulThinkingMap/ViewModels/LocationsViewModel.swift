58a6e6017048617de6cd2f98555ba5ad80cf5027
James Greiner <jamesdgreiner@icloud.com>
2023-07-14 19:30:38 -0700

    Update view model to manage map locations and map region

--- SwiftfulThinkingMap/ViewModels/LocationsViewModel.swift
+++ SwiftfulThinkingMap/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,35 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(locations.first!)
+    }
+    
+    private func updateMapRegion(_ location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
     }
 }
